c943e3e8df92a1b8e66394ee09a501625746077f
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-04-15 22:40:57 +0100

    create c++ solution

--- merge_strings_alternately/merge_strings_alternately.hpp
+++ merge_strings_alternately/merge_strings_alternately.hpp
@@ -1,3 +1,5 @@
+#include <ranges>
+
 class Solution
 {
 public:
@@ -21,6 +23,30 @@ public:
         else if (i < word2.size())
         {
             out_s.append(word2.begin() + i, word2.end());
+        }
+        // return the new string
+        return out_s;
+    }
+
+    std::string mergeAlternately(std::string word1, std::string word2)
+    {
+
+        std::basic_string<char> out_s;              // make a string object
+        out_s.reserve(word1.size() + word2.size()); // reserve the size
+        // zip and interleave
+        for (const auto &[a, b] : std::ranges::views::zip(word1, word2))
+        {
+            out_s.push_back(a);
+            out_s.push_back(b);
+        }
+        // append the remaining characters
+        if (word1.size() > word2.size())
+        {
+            out_s.append(word1.begin() + std::min(word1.size(), word2.size()), word1.end());
+        }
+        else
+        {
+            out_s.append(word2.begin() + std::min(word1.size(), word2.size()), word2.end());
         }
         // return the new string
         return out_s;
