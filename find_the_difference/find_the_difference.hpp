e23931877866ac02c1f7121f5c66f30d6d6164b8
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-04-18 12:16:10 +0100

    create c++ solution

---
+++ find_the_difference/find_the_difference.hpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    char findTheDifference(string s, string t)
+    {
+        // find the difference between sets
+        std::multiset<char> s_set(s.begin(), s.end());
+        std::multiset<char> t_set(t.begin(), t.end());
+        std::vector<char> o_vec;
+        std::set_symmetric_difference(s_set.begin(), s_set.end(), t_set.begin(), t_set.end(), std::back_inserter(o_vec));
+        return o_vec.at(0);
+    }
+};
