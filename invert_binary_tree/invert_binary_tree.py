45e37e46debb131cdde9fac79670e0cf7ef63ebc
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-04-28 22:05:26 +0100

    update python solution

--- invert_binary_tree/invert_binary_tree.py
+++ invert_binary_tree/invert_binary_tree.py
@@ -13,3 +13,26 @@ class Solution:
             self.invertTree(root.left)
             self.invertTree(root.right)
         return root
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # iterative approach
+        # emulate the call-stack
+        todo_stack = [root]
+        while todo_stack:
+            node = todo_stack.pop()
+            if node:
+                # swap the subtrees
+                node.left, node.right = node.right, node.left
+                # come back to these subtrees in a moment
+                todo_stack.append(node.left)
+                todo_stack.append(node.right)
+        return root
