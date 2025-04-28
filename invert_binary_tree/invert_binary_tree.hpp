a4b20a2abab05b0e69fd297aef7c87de73a370cb
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-04-28 22:05:10 +0100

    update c++ solution

--- invert_binary_tree/invert_binary_tree.hpp
+++ invert_binary_tree/invert_binary_tree.hpp
@@ -25,3 +25,40 @@ public:
         return root;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        // iterative approach
+        // emulate the call-stack
+        std::stack<TreeNode *> todo_stack;
+        todo_stack.push(root);
+        while (!todo_stack.empty())
+        {
+            auto node = todo_stack.top();
+            todo_stack.pop();
+            if (node != nullptr)
+            {
+                // swap the subtrees
+                std::swap(node->left, node->right);
+                // come back to these subtrees in a moment
+                todo_stack.push(node->left);
+                todo_stack.push(node->right);
+            }
+        }
+        return root;
+    }
+};
